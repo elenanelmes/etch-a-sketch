073cf1e8e8b28ef5f7f781bce25ce2b448e0f60f
Elena Nelmes <elena.nelmes@gmail.com>
2024-08-06 17:15:44 +0300

    add basic functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+const body = document.querySelector('#body');
+const cells = document.querySelectorAll('.cell');
+const btnReset = document.querySelector('#btn-reset');
+
+const GRID_SIZE = 16;
+
+function createGrid() {
+    const grid = createElement('div', { id: 'grid' });
+    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+        const cell = createElement('div', { id: i, class: 'cell' });
+        cell.addEventListener('mouseover', changeCellColour);
+        grid.appendChild(cell);
+    }
+    body.appendChild(grid);
+}
+
+createGrid();
+
+btnReset.addEventListener("click", resetGrid);
+
+function changeCellColour(e) {
+    e.target.classList.add('fill');
+}
+
+function resetGrid() {
+    const cells = 
+    cells.forEach(cell => {
+        cell.classList.remove('fill');
+    });
+}
+
+function createElement(tag, attributes = {}, textContent = '') {
+    const element = document.createElement(tag);
+    Object.keys(attributes).forEach(attr => element.setAttribute(attr, attributes[attr]));
+    if (textContent) element.textContent = textContent;
+    return element;
+}
